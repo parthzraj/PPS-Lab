f0cf8d1f4c75a435caff38c59fb43804c6fb4f1f
parthzraj <parthraj.edu@gmail.com>
2026-03-25 03:06:48 +0530

    Palindrome

--- 16palindrome.py
+++ 16palindrome.py
@@ -1,10 +1,10 @@
-text = input("Enter a string: ")
+texts = input("Enter a string: ")
 
 
-reverse_text = text[::-1]
+reverse_text = texts[::-1]
 
 
-if text == reverse_text:
+if texts == reverse_text:
     print("The string is a Palindrome")
 else:
     print("The string is NOT a Palindrome")

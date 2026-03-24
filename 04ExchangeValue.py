68aad16f231fdef20820b6357fe6d7aac2af1f9d
parthzraj <parthraj.edu@gmail.com>
2026-03-25 01:46:59 +0530

    Swapping indexes of the list

---
+++ 04ExchangeValue.py
@@ -0,0 +1,11 @@
+lst = list(map(int, input("Enter list elements: ").split()))
+
+i = int(input("Enter first index: "))
+j = int(input("Enter second index: "))
+
+
+if 0 <= i < len(lst) and 0 <= j < len(lst):
+    lst[i], lst[j] = lst[j], lst[i]
+    print("Updated list:", lst)
+else:
+    print("Invalid indices!")
